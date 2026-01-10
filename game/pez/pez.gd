44651c79f5cd0ec860210fb7df76197d4e599fb2
ferranrubert <ferranrubert@gmail.com>
2026-01-10 14:59:38 +0100

    fish movement drag and drop

--- game/pez/pez.gd
+++ game/pez/pez.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 #region Caracteristicas de los peces
 enum NUM_OJOS {
@@ -37,6 +37,8 @@ enum TIPO_COLA {
 #endregion
 
 @export var PID: int = -1
+@export var gravity := 1200.0
+@export var drag_speed := 20.0
 @export var textura: Texture
 @export var ojos: NUM_OJOS
 @export var cabeza: ESTADO_CABEZA
@@ -44,14 +46,22 @@ enum TIPO_COLA {
 @export var cola: TIPO_COLA
 
 
+var dragging: bool = false
+var grab_offset := Vector2.ZERO
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	pass
+	if dragging:
+		var target := get_global_mouse_position() - grab_offset
+		velocity = (target - global_position) * drag_speed
+		move_and_slide()
+	else:
+		velocity.y += gravity * delta
+		move_and_slide()
 
 
 func _on_corte_cabeza_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
@@ -70,3 +80,15 @@ func _on_corte_cola_input_event(_viewport: Node, event: InputEvent, _shape_idx: 
 	if event.is_pressed() and event.button_index == MOUSE_BUTTON_RIGHT:
 		#TODO Hay que cortar la textura de la cola de alguna forma y cambiar su estado a cortada
 		pass
+
+			
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+			dragging = false
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+		if Input.is_action_just_pressed("click") and not dragging:
+			dragging = true
+			grab_offset = get_global_mouse_position() - global_position
+			print("selected")
