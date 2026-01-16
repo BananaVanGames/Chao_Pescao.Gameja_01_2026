20533d9aaa6b2399a4156a361a784f7b45290060
Adrián <adrian.lin.xiang@gmail.com>
2026-01-16 22:47:50 +0100

    Sistema de puntuación arreglado

--- game/score_zone/score_zone.gd
+++ game/score_zone/score_zone.gd
@@ -20,11 +20,6 @@ const CUERPO_ENFERMO = 1
 #endregion
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 func _on_good_area_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("pez"):
 		return
@@ -63,6 +58,7 @@ func _on_good_area_body_entered(body: Node2D) -> void:
 			# No debía cortarse pero se cortó
 			score -= 1
 	print("Score después de la cabeza: ", score)
+
 	if score != -3:
 		if fish_data[COLA_PEZ] == current_rules[COLA_RULES]:
 			if not processable_rules[COLA_RULES]:
@@ -96,59 +92,43 @@ func _on_bad_area_body_entered(body: Node2D) -> void:
 	print("Current rules: ", current_rules)
 	print("Danger rules: ", processable_rules)
 
-	#Con este IF nos cercioramos de que el pez tiene al menos 1 parte tóxica
-	if (
-			fish_data[CUERPO_PEZ] == CUERPO_SANO
-			or (
-				processable_rules[OJOS_RULES]
-				and processable_rules[COLA_RULES]
-				and processable_rules[CABEZA_RULES]
-				and current_rules[CABEZA_RULES] != CABEZA_SANA
-			)
+	if not (
+			fish_data[OJOS_PEZ] > current_rules[OJOS_RULES] and not processable_rules[OJOS_PEZ]
+			or (fish_data[CABEZA_PEZ] == current_rules[CABEZA_RULES] and not processable_rules[CABEZA_RULES]
+				and fish_data[CABEZA_PEZ] != CABEZA_SANA)
+			or fish_data [CUERPO_PEZ] == CUERPO_ENFERMO
+			or fish_data[COLA_PEZ] == current_rules[COLA_RULES] and not processable_rules[COLA_RULES]
 	):
 		GameHandler.add_score(-3)
 		body.queue_free()
-
-	if (
-			fish_data[OJOS_PEZ] <= current_rules[OJOS_RULES] and not processable_rules[OJOS_RULES]
-			or fish_data[CABEZA_PEZ] == current_rules[CABEZA_RULES] and not processable_rules[CABEZA_RULES]
-			or fish_data[COLA_PEZ] == current_rules[COLA_RULES] and not processable_rules[COLA_RULES]
-	):
-		pass
+		return
 
 	var score := 2
 	var cabeza_mal := false
 
-	if fish_data[OJOS_PEZ] > current_rules[OJOS_RULES]:
-		cabeza_mal = true
-	if fish_data[CABEZA_PEZ] == current_rules[CABEZA_RULES] and fish_data[CABEZA_PEZ] != 0:
-		cabeza_mal = true
+	if not fish_data [CUERPO_PEZ] == CUERPO_ENFERMO:
+		if fish_data[OJOS_PEZ] > current_rules[OJOS_RULES]:
+			cabeza_mal = true
+		if fish_data[CABEZA_PEZ] == current_rules[CABEZA_RULES] and fish_data[CABEZA_PEZ] != CABEZA_SANA:
+			cabeza_mal = true
 
-	var cabeza_cortada = fish_data[CABEZA_CORTADA]
+		var cabeza_cortada = fish_data[CABEZA_CORTADA]
 
-	if cabeza_mal:
-		if processable_rules[0]:
-			if cabeza_cortada:
-				score -= 1
-	else:
-		if cabeza_cortada:
-			score -= 1
+		if cabeza_mal:
+			if not processable_rules[OJOS_RULES] or not processable_rules[CABEZA_RULES]:
+				if cabeza_cortada:
+					score -= 2
+		print("Score después de la cabeza: ", score)
 
-	print("Score después de la cabeza: ", score)
-	if score != -3:
 		if fish_data[COLA_PEZ] == current_rules[COLA_RULES]:
-			if not processable_rules[2]:
-				score = -3
-			else:
-				if not fish_data[COLA_CORTADA]:
-					score -= 1
-		else:
-			if fish_data[COLA_CORTADA]:
-				score -= 1
-	print("Score después de la cola: ", score)
+			if not processable_rules[COLA_RULES]:
+				if fish_data[COLA_CORTADA]:
+					score -= 2
+		print("Score después de la cola: ", score)
 
 	if score == 2 and GameHandler.get_time() > 1.00:
-		#print("Punto extra por velocidad")
 		score = 3
 
+	print("Puntuación final añadida: ", score, "\n")
+	GameHandler.add_score(score)
 	body.queue_free()
